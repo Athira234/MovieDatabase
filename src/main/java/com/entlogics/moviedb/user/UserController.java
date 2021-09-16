596c586748560f99f0e91e4aea3ac0d20ebae59a
athira234 <athira@entlogics.com>
2021-09-16 19:37:31 +0530

    Added empty methods in UserController

---
+++ src/main/java/com/entlogics/moviedb/user/UserController.java
@@ -0,0 +1,101 @@
+package com.entlogics.moviedb.user;
+
+public class UserController {
+
+	//Method for rating a movie
+	public String rateMovie()
+	{
+		return null;
+	}
+	
+	//Method for  giving  feedback for a movie
+	
+	public String giveMovieFeedback()
+	{
+		return null;
+	}
+	//Method for  adding a movie to watchlist
+	public String addMovieToWatchlist()
+	{
+		return null;
+	}
+	
+	//Method for  adding movie to favourites
+	public String addMovieToFavourites()
+	{
+		return null;
+	}
+	
+	//Method for recommending a movie to another user
+	public String recommendMovie()
+	{
+		return null;
+	}
+	
+	//Method for viewing watchlist
+	public String viewWatchList()
+	{
+		return null;
+	}
+	
+	//Method for viewing  favourites
+	public String viewFavourites()
+	{
+		return null;
+	}
+	
+	//Method for viewing  ratings
+	public String viewRatings()
+	{
+		return null;
+	}
+	
+	//Method for viewing feedback
+	
+	public String viewFeedbacks()
+	{
+		return null;
+	}
+	
+	//Method for viewing his/her profile
+	public String viewProfile()
+	{
+		return null;
+	}
+	
+	
+	//Method for  editing his/her profile
+	public String editProfileform()
+	{
+		return null;
+	}
+	
+	
+	//Method for  changing password
+	public String updatePassword()
+	{
+		return null;
+	}
+	
+	//Method for signs up for a new account
+	public String signUpform()
+	{
+		return null;
+	}
+	
+	//Method for  login
+	public String login()
+	{
+		return null;
+	}
+	//Method for  logouts of the system
+	
+	public String logout()
+	{
+		return null;
+	}
+	
+	
+	
+	
+}

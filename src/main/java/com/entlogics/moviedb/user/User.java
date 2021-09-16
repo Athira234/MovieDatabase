039dd9d2e3ab74be6901350a5cc683266d4066a8
athira234 <athira@entlogics.com>
2021-09-16 18:35:50 +0530

    Designed User Model

--- src/main/java/com/entlogics/moviedb/user/User.java
+++ src/main/java/com/entlogics/moviedb/user/User.java
@@ -1,5 +1,109 @@
 package com.entlogics.moviedb.user;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dt_user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	private int userId;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "username")
+	private String userName;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "role")
+	private String role;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	List<UserMovie> moviesOfUser;
+
+	@OneToMany(mappedBy = "userActivity", cascade = CascadeType.ALL)
+	private List<UserActivity> userActivities;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", userName=" + userName + ", email=" + email + ", role=" + role + ", moviesOfUser="
+				+ moviesOfUser + "]";
+	}
 
 }

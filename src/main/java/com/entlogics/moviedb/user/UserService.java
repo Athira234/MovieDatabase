4e205200918ec531847502e447f6f1a1967fb5c9
athira234 <athira@entlogics.com>
2021-09-16 19:44:24 +0530

    Modified UserService

--- src/main/java/com/entlogics/moviedb/user/UserService.java
+++ src/main/java/com/entlogics/moviedb/user/UserService.java
@@ -4,84 +4,95 @@ import java.util.List;
 
 import com.entlogics.moviedb.movie.Movie;
 
-
-
-public class UserService {
+public class UserService implements IUserService {
 	// Method for rating a movie
-	void rateMovie(UserMovie userMovie) {
-		
+	@Override
+	public void rateMovie(UserMovie userMovie) {
+
 	}
 
 	// Method for giving feedback for a movie
-
-	void giveMovieFeedback(UserMovie userMovie) {
+	@Override
+	public void giveMovieFeedback(UserMovie userMovie) {
 		// return null;
 	}
 
 	// Method for adding a movie to watchlist
-	void addMovieToWatchlist(UserWatchList watchList, Movie movie) {
-		//return null;
+	@Override
+	public void addMovieToWatchlist(UserWatchList watchList, Movie movie) {
+		// return null;
 	}
 
 	// Method for adding movie to favourites
-	void  addMovieToFavourites(UserMovie userMovie) {
-		//return null;
+	@Override
+	public void addMovieToFavourites(UserMovie userMovie) {
+		// return null;
 	}
 
 	// Method for recommending a movie to another user
-	void recommendMovie(UserMovie userMovie) {
-		//return null;
+	@Override
+	public void recommendMovie(UserMovie userMovie) {
+		// return null;
 	}
 
 	// Method for viewing watchlist
-	List<UserWatchListItems> getWatchList(int userId,int watchListId) {
+	@Override
+	public List<UserWatchListItems> getWatchList(int userId, int watchListId) {
 		return null;
 	}
 
 	// Method for viewing favourites
-	List<UserMovie> getFavourites(int userId) {
+	@Override
+	public List<UserMovie> getFavourites(int userId) {
 		return null;
 	}
 
 	// Method for viewing ratings
-	List<UserMovie> viewRatings(int userId,int movieId) {
+	@Override
+	public List<UserMovie> viewRatings(int userId, int movieId) {
 		return null;
 	}
 
 	// Method for viewing feedback
-
-	List<UserMovie> viewFeedbacks(int userId,int movieId) {
+	@Override
+	public List<UserMovie> viewFeedbacks(int userId, int movieId) {
 		return null;
 	}
 
 	// Method for viewing his/her profile
-	List<User> viewProfile(int userId) {
+	@Override
+	public List<User> viewProfile(int userId) {
 		return null;
 	}
 
 	// Method for editing his/her profile
-       void editProfile(User user,int userId) {
-		
+	@Override
+	public void editProfile(User user, int userId) {
+
 	}
 
 	// Method for changing password
-	void updatePassword(User user) {
-		
+	@Override
+	public void updatePassword(User user) {
+
 	}
 
 	// Method for signs up for a new account
-	void signUp() {
-		
+	@Override
+	public void signUp() {
+
 	}
 
 	// Method for login
-	void login() {
-		
-	}
-	// Method for logouts of the system
+	@Override
+	public void login() {
 
-	void logout() {
-		
+	}
+
+	// Method for logouts of the system
+	@Override
+	public void logout() {
+
 	}
 
 }

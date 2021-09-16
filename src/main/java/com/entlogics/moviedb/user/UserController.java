fd0a10f246e394500f84df15ecce1fafc8d7c4c8
athira234 <athira@entlogics.com>
2021-09-16 21:18:14 +0530

    Added sysouts in UserController

--- src/main/java/com/entlogics/moviedb/user/UserController.java
+++ src/main/java/com/entlogics/moviedb/user/UserController.java
@@ -5,48 +5,56 @@ public class UserController {
 	//Method for rating a movie
 	public String rateMovie()
 	{
+		System.out.println("Inside rateMovie() method in UserController");
 		return null;
 	}
 	
 	//Method for  giving  feedback for a movie
 	
 	public String giveMovieFeedback()
-	{
+	{ 
+		System.out.println("Inside giveMovieFeedback() method in UserController");
 		return null;
 	}
 	//Method for  adding a movie to watchlist
 	public String addMovieToWatchlist()
 	{
+		System.out.println("Inside addMovieToWatchlist() method in UserController");
 		return null;
 	}
 	
 	//Method for  adding movie to favourites
 	public String addMovieToFavourites()
 	{
+		System.out.println("Inside addMovieToFavourites() method in UserController");
 		return null;
 	}
 	
 	//Method for recommending a movie to another user
 	public String recommendMovie()
 	{
+		System.out.println("Inside recommendMovie() method in UserController");
 		return null;
 	}
 	
 	//Method for viewing watchlist
 	public String viewWatchList()
 	{
+		System.out.println("Inside viewWatchList method in UserController");
 		return null;
 	}
 	
 	//Method for viewing  favourites
 	public String viewFavourites()
 	{
+		System.out.println("Inside viewFavourites method in UserController");
 		return null;
 	}
 	
 	//Method for viewing  ratings
 	public String viewRatings()
 	{
+		System.out.println("Inside viewRatings method in UserController");
 		return null;
 	}
 	
@@ -54,12 +62,14 @@ public class UserController {
 	
 	public String viewFeedbacks()
 	{
+		System.out.println("Inside viewFeedbacks method in UserController");
 		return null;
 	}
 	
 	//Method for viewing his/her profile
 	public String viewProfile()
 	{
+		System.out.println("Inside viewProfile method in UserController");
 		return null;
 	}
 	
@@ -67,6 +77,7 @@ public class UserController {
 	//Method for  editing his/her profile
 	public String editProfileform()
 	{
+		System.out.println("Inside editProfileform method in UserController");
 		return null;
 	}
 	
@@ -74,24 +85,28 @@ public class UserController {
 	//Method for  changing password
 	public String updatePassword()
 	{
+		System.out.println("Inside updatePassword method in UserController");
 		return null;
 	}
 	
 	//Method for signs up for a new account
 	public String signUpform()
 	{
+		System.out.println("Inside signUpform() method in UserController");
 		return null;
 	}
 	
 	//Method for  login
 	public String login()
 	{
+		System.out.println("Inside login() method in UserController");
 		return null;
 	}
 	//Method for  logouts of the system
 	
 	public String logout()
 	{
+		System.out.println("Inside logout() method in UserController");
 		return null;
 	}
 	

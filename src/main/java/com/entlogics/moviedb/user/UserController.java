64015b2eddf14f5b5e58fe6bf8951ad3341408b0
athira234 <athira@entlogics.com>
2021-09-23 17:19:28 +0530

    Implemented methods in Usercontroller

--- src/main/java/com/entlogics/moviedb/user/UserController.java
+++ src/main/java/com/entlogics/moviedb/user/UserController.java
@@ -1,137 +1,156 @@
 package com.entlogics.moviedb.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
 @Controller
+
 public class UserController {
-	
+
 	IUserService iUserService;
-	
-    @Autowired
+
+	@Autowired
 	public void setiUserService(IUserService iUserService) {
 		this.iUserService = iUserService;
 	}
 
-	//Method for rating a movie
-    @RequestMapping("/users/{userId}/movies/{movieId}/ratings")
-	public String rateMovieForm(@PathVariable int userId)
-	{
+	// Method for rating a movie
+	@RequestMapping("/movies/{movieId}/ratings")
+	public String rateMovieForm(@PathVariable int userId, Model model) {
 		System.out.println("Inside rateMovie() method in UserController");
-		//User user=iUserService.getUser();
-		iUserService.rateMovie(1,1);
+		// User user=iUserService.getUser();
+
+	
 		return null;
 	}
-	
-	//Method for  giving  feedback for a movie
-	
-	public String giveMovieFeedback()
-	{ 
+
+	// Method for giving feedback for a movie
+
+	public String giveMovieFeedback() {
 		System.out.println("Inside giveMovieFeedback() method in UserController");
 		return null;
 	}
-	//Method for  adding a movie to watchlist
-	public String addMovieToWatchlist()
-	{
+
+	// Method for adding a movie to watchlist
+	public String addMovieToWatchlist() {
 		System.out.println("Inside addMovieToWatchlist() method in UserController");
 		return null;
 	}
-	
-	//Method for  adding movie to favourites
-	public String addMovieToFavourites()
-	{
+
+	// Method for adding movie to favourites
+	public String addMovieToFavourites() {
 		System.out.println("Inside addMovieToFavourites() method in UserController");
 		return null;
 	}
-	
-	//Method for recommending a movie to another user
-	public String recommendMovie()
-	{
+
+	// Method for recommending a movie to another user
+	public String recommendMovie() {
 		System.out.println("Inside recommendMovie() method in UserController");
 		return null;
 	}
-	
-	//Method for viewing watchlist
-	public String viewWatchList()
-	{
+
+	// Method for viewing watchlist
+
+	@RequestMapping("/users/{userId}/watchlist")
+	public String viewWatchList(@PathVariable int userId, Model model) {
 		System.out.println("Inside viewWatchList method in UserController");
-		return null;
+		List<UserWatchListItems> watchListItems = iUserService.getWatchList(userId);
+		System.out.println("WatchListItems=" + watchListItems);
+		model.addAttribute("watchList", watchListItems);
+		return "watchlist";
+
 	}
-	
-	//Method for viewing  favourites
-	public String viewFavourites()
-	{
+
+	// Method for viewing favourites
+	public String viewFavourites() {
 		System.out.println("Inside viewFavourites method in UserController");
 		return null;
 	}
-	
-	//Method for viewing  ratings
-	public String viewRatings()
-	{
+
+	// Method for viewing ratings
+	public String viewRatings() {
 		System.out.println("Inside viewRatings method in UserController");
 		return null;
 	}
-	
-	//Method for viewing feedback
-	
-	public String viewFeedbacks()
-	{
+
+	// Method for viewing feedback
+
+	public String viewFeedbacks() {
 		System.out.println("Inside viewFeedbacks method in UserController");
 		return null;
 	}
-	
-	//Method for viewing his/her profile
+
+	// Method for viewing his/her profile
 	@RequestMapping("/users/{userId}/profile")
-	public String viewProfile(@PathVariable int userId, Model model)
-	{
+	public String viewProfile(@PathVariable int userId, Model model) {
 		System.out.println("Inside viewProfile method in UserController");
 		User user = iUserService.getProfile(userId);
 		model.addAttribute("user", user);
 		return "user-profile";
- 	}
-	
-	
-	//Method for  editing his/her profile
-	public String editProfileform()
-	{
+	}
+
+	// Method for editing his/her profile
+	@RequestMapping(value="/users/{userId}/editprofile")
+	public String editProfileform(@PathVariable int userId, Model m) {
 		System.out.println("Inside editProfileform method in UserController");
-		return null;
+		User user=iUserService.getProfile(userId);
+		m.addAttribute("user",user);
+		return "edit-user-profile";
+	
 	}
-	
-	
-	//Method for  changing password
-	public String updatePassword()
-	{
+	@RequestMapping("/users/{userId}/editprofile/editdetails")
+	public String updateUser(@ModelAttribute("user") User user) {
+		System.out.println("Inside updateUser() method in SchoolController ");
+		User user1=iUserService.getProfile(user.getUserId());
+		user1.setFirstName(user.getFirstName());
+		user1.setLastName(user.getLastName());
+		user1.setEmail(user.getEmail());
+		iUserService.editProfile(user1);
+		return "success";
+	}
+	// Method for changing password
+	@RequestMapping(value="/users/{userId}/password")
+	public String updatePassword(@PathVariable int userId, Model m) {
 		System.out.println("Inside updatePassword method in UserController");
-		return null;
+		User user=iUserService.getProfile(userId);
+		m.addAttribute("user",user);
+		return "edit-user-password-form";
+		
 	}
-	
-	//Method for signs up for a new account
-	public String signUpform()
-	{
+	@RequestMapping("/users/{userId}/password/editdetails")
+	public String updatePassword(@ModelAttribute("user") User user) {
+		System.out.println("Inside updateUser() method in SchoolController ");
+		User user1=iUserService.getProfile(user.getUserId());
+		user1.setPassword(user.getPassword());
+		iUserService.updatePassword(user1);
+		return "success";
+	}
+
+	// Method for signs up for a new account
+	public String signUpform() {
 		System.out.println("Inside signUpform() method in UserController");
 		return null;
 	}
-	
-	//Method for  login
-	public String login()
-	{
+
+	// Method for login
+	public String login() {
 		System.out.println("Inside login() method in UserController");
 		return null;
 	}
-	//Method for  logouts of the system
-	
-	public String logout()
-	{
+	// Method for logouts of the system
+
+	public String logout() {
 		System.out.println("Inside logout() method in UserController");
 		return null;
 	}
-	
-	
-	
-	
+
 }

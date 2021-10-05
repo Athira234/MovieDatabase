50be2f5b03b7cbad1375fa9af55f7fd78cba4087
athira234 <athira@entlogics.com>
2021-10-05 18:35:17 +0530

    Add empty  Methods in MovieService

--- src/main/java/com/entlogics/moviedb/movie/service/MovieService.java
+++ src/main/java/com/entlogics/moviedb/movie/service/MovieService.java
@@ -1,5 +1,92 @@
 package com.entlogics.moviedb.movie.service;
 
-public class MovieService {
+import java.util.List;
+
+import com.entlogics.moviedb.movie.entities.Movie;
+import com.entlogics.moviedb.movie.entities.MovieCast;
+import com.entlogics.moviedb.movie.entities.MovieCrew;
+import com.entlogics.moviedb.user.entities.UserMovie;
+
+public class MovieService implements IMovieService {
+
+	@Override
+	public List<Movie> getTopRatingMovies() {
+
+		return null;
+	}
+
+	@Override
+	public List<Movie> getHighestRatingMovies() {
+
+		return null;
+	}
+
+	@Override
+	public List<Movie> getTopRecommendedMovies() {
+
+		return null;
+	}
+
+	@Override
+	public List<UserMovie> getRatingsOfMovie(int MovieId) {
+
+		return null;
+	}
+
+	@Override
+	public List<MovieCast> getCastOfMovie(int MovieId) {
+
+		return null;
+	}
+
+	@Override
+	public List<MovieCrew> getCrewOfMovie(int MovieId) {
+
+		return null;
+	}
+
+	@Override
+	public void addCast(MovieCast movieCast) {
+
+	}
+
+	@Override
+	public void addCrew(MovieCrew movieCrew) {
+
+	}
+
+	@Override
+	public void deleteCast(int personId) {
+
+	}
+
+	@Override
+	public void deleteCrew(int personId) {
+
+	}
+
+	@Override
+	public List<Movie> movieByCompany(int companyId) {
+
+		return null;
+	}
+
+	@Override
+	public List<Movie> movieByActor(int personId) {
+
+		return null;
+	}
+
+	@Override
+	public List<Movie> movieByGenre(int genreId) {
+
+		return null;
+	}
+
+	@Override
+	public List<Movie> movieByTitle(String title) {
+
+		return null;
+	}
 
 }

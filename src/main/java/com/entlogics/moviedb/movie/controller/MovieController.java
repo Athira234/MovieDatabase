0fa33d2c561fdce13c7b7fe12a4261d14bd8aade
athira234 <athira@entlogics.com>
2021-10-05 18:32:47 +0530

    Add empty methods in Movie Controller

--- src/main/java/com/entlogics/moviedb/movie/controller/MovieController.java
+++ src/main/java/com/entlogics/moviedb/movie/controller/MovieController.java
@@ -2,4 +2,88 @@ package com.entlogics.moviedb.movie.controller;
 
 public class MovieController {
 
+	// method to find toprating movies
+	public String getTopRatingMovies() {
+		return null;
+
+	}
+
+	// method to find highest gross Movies
+	public String getHighestRatingMovies() {
+		return null;
+
+	}
+
+	// method to find Top Recommended Movies
+	public String getTopRecommendedMovies() {
+		return null;
+
+	}
+
+	// method to find ratings of a Movie
+	public String getRatingsOfMovie() {
+		return null;
+
+	}
+
+	// method to find cast of a Movie
+	public String getCastOfMovie() {
+		return null;
+
+	}
+
+	// method to find crew of a Movie
+	public String getCrewOfMovie() {
+		return null;
+
+	}
+
+	// method to add cast to movie
+	public String addCast() {
+		return null;
+
+	}
+
+	// method to add crew to movie
+	public String addCrew() {
+		return null;
+
+	}
+
+	// method to delete person from cast
+	public String deleteCast() {
+		return null;
+
+	}
+
+	// method to delete person from crew
+	public String deleteCrew() {
+		return null;
+
+	}
+
+	// method to get Movie by Company
+	public String movieByCompany() {
+		return null;
+
+	}
+
+	// method to get Movie by actor
+	public String movieByActor() {
+		return null;
+
+	}
+
+	// method to get Movie by genre
+	public String movieByGenre() {
+		return null;
+
+	}
+
+	// method to get Movie by Title
+	public String movieByTitle() {
+		return null;
+
+	}
+
 }
